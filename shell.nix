b7f9a264fb21b09fe8cc5e07a2822307fe874a77
Joris <joris@guyonvarch.me>
2019-10-30 15:31:36 +0100

    Upgrade to lastest elm-compiler

--- shell.nix
+++ shell.nix
@@ -1,16 +1,16 @@
 let
   elm019Env = fetchGit {
      url = "git@github.com:dividat/elm-compiler.git";
-     rev = "8ae2148630d1ac7bc6d26c4433ae32124a5311fb";
+     ref = "master";
   };
 in
 (import elm019Env {
-   elmPackages = (import ./tests/elm-srcs.nix) // (import ./example/elm-srcs.nix);
-   watchPkg = "dividat/elm-binary/1.1.0";
-   privatePackages = {
-      "dividat/elm-binary" = {
-          src = ./.;
-          version = "1.1.0";
-      };
-   };
+   watchPkg = "dividat/elm-binary/1.2.0";
+   dependenciesFrom = [
+      ./tests/elm.json
+      ./example/elm.json
+   ];
+   dependencySources = [
+      ./.
+   ];
 }).shell
