737e0c7901a99e9c283721ea85df51eb66733daa
Emilien Taque <emilien@dividat.ch>
2021-12-10 15:29:03 +0100

    Upgrade to nixpkgs 21.11 (step 1)

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 let
 
   pkgs = (import (builtins.fetchGit {
-    url = "git@github.com:dividat/elm-compiler.git";
-    rev = "6f755abe65739990a2641fbea8775704eb8d4f35"; # 4.0.0
+    url = "git@github.com:etaque/elm-compiler.git";
+    rev = "7cef1de9207b241675432ae40d723b6adf1024dd"; # 4.0.0
   })).forPackage {
     hasTests = true;
     hasExamples = true;
